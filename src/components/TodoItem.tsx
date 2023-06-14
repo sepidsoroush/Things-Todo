fc06640df5a358ffa1e161a0d11753497b78577b
Sepideh Soroush <105380339+sepidsoroush@users.noreply.github.com>
2023-06-14 17:47:06 +0300

    added styles

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import classes from "./NewTodo.module.css";
 
 const TodoItem: React.FC<{ todoText: string }> = (props) => {
-  return <li>{props.todoText}</li>;
+  return <li className={classes.item}>{props.todoText}</li>;
 };
 
 export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Todo from "../models/todos";
 import TodoItem from "./TodoItem";
+import classes from "./Todos.module.css";
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         <TodoItem key={item.id} todoText={item.text} />
       ))}
